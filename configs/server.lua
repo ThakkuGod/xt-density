df7b9f8cde61fd33c49454634aa46f1cb5092925
ThakkuGod <103792673+ThakkuGod@users.noreply.github.com>
2024-11-25 06:01:29 +0530

    Update server.lua

--- configs/server.lua
+++ configs/server.lua
@@ -1,5 +1,4 @@
 return {
-    requiredPermission = 'admin',       -- Required ace permission to use the menu
     commandName = 'density',            -- Command to open the menu
     defaultValues = {                   -- Default density values
         ['parked'] = 0.8,
